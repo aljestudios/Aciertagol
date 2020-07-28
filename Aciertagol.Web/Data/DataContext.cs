d05b5c1f4213f3d16f63329be426860ad5a602a3
axel de jesus <aljestudios@hotmail.com>
2020-07-27 19:19:19 -0500

    Add Team Controller

---
+++ Aciertagol.Web/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Aciertagol.Web.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aciertagol.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        public DbSet<TeamEntity> Teams { get; set; }
+    }
+}
